4cd8618bed9b55b3e354550a730fd370abb0aeac
Jeff Hughes <jeff.hughes@gmail.com>
2015-11-22 16:39:57 -0500

    Add headers and help text.

--- ui.R
+++ ui.R
@@ -15,6 +15,7 @@ shinyUI(fluidPage(
         
         # Sidebar
         sidebarPanel(
+            h4('Test information', style='margin-top: 0'),
             selectInput('tType', label=HTML('Type of <i>t</i>-test'), choices=list(
                 'One sample' = 'onesamp',
                 'Independent samples' = 'indep',
@@ -27,6 +28,7 @@ shinyUI(fluidPage(
             
             checkboxInput('onetail', label='One-tailed?'),
             
+            h4('Set prior', style='margin-top: 2em'),
             selectInput('prior', label='Select a prior:', choices=list(
                 'small (.2)' = 'small',
                 'medium (.5)' = 'medium',
@@ -43,7 +45,12 @@ shinyUI(fluidPage(
                 textInput('power',
                     label=HTML('If determining prior from <i>N</i>, input power here:'),
                     value='.8')
-            )
+            ),
+            
+            helpText('The prior is set to a Cauchy distribution with a scaling',
+                'width equal to the expected effect size. You can either select',
+                'a preset effect size, input your own, or infer the expected',
+                'effect size based on the power of the test.')
         ),
         
         # Main panel
